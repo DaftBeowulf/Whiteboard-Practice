d8b254161f83f61c33bb0c6f798b9149a6d39496
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-10-03 23:28:17 -0500

    :sparkles: O(n) runtime for ProductOf...

--- ProductOfAllOtherNumbers/product.py
+++ ProductOfAllOtherNumbers/product.py
@@ -17,13 +17,32 @@ by calculating
 """
 
 
+# def product_of_others(arr):
+#     result = [1]*len(arr)
+#     for index, val in enumerate(arr):
+#         for sub_i, sub_v in enumerate(arr):
+#             if sub_i != index:
+#                 result[index] *= sub_v
+#     return result
+# O(n^2)
+
 def product_of_others(arr):
-    result = [1]*len(arr)
-    for index, val in enumerate(arr):
-        for sub_i, sub_v in enumerate(arr):
-            if sub_i != index:
-                result[index] *= sub_v
+    result = [0]*len(arr)
+
+    # get product of all values after given index
+    temp_product = 1
+    for i in range(0, len(arr)):
+        result[i] = temp_product
+        temp_product *= arr[i]
+
+    # *= by product of all values before index -- thereby product of all before/after values
+    temp_product = 1
+    for j in range(len(arr)-1, -1, -1):
+        result[j] *= temp_product
+        temp_product *= arr[j]
     return result
+
+# O(2n) -> O(n)
 
 
 print(product_of_others([1, 7, 3, 4]))
