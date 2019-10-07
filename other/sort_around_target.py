f48e5a401f0f4c453bff0bdfc10cee0cfb1b8c0d
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-10-07 00:11:13 -0500

    :sparkles: Prompt for sort_around_target

---
+++ other/sort_around_target.py
@@ -0,0 +1,10 @@
+"""
+given an array and a target, sort the array where the numbers smaller 
+than the target are sorted to the left side of the target and numbers 
+larger than the target are sorted to the right side of the target
+ex:
+input:[6,5,2,3,8], 5
+output : [2,3,5,6,8]
+"""
+# 4 potential approaches
+# brute force #1:
